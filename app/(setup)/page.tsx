28cb05ce6f56beb8b971da4439c9833444c65611
Beaskyy <afeezakintobi@nqb8.co>
2023-09-25 23:07:14 +0100

    severId

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,8 +1,25 @@
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import { InitialModal } from "@/components/ui/modals/initial-modal";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
-  return <div>Create a server</div>;
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  return <InitialModal />;
 };
 
 export default SetupPage;
